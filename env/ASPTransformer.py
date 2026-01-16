fixed lake dist calculation; fixed lake timer; added frog information to ctd

env/ASPTransformer.py
@@ -74,14 +74,15 @@ class ASPTransformer:
                             continue
                         if dist[nx, ny] > dist[x, y] + 1:
                             dist[nx, ny] = dist[x, y] + 1
-                            #todo: not sure if this is correct
                             if dist[x, y] == 0:
-                                # Immediate neighbor: The best step is the current move
-                                best[nx, ny] = np.array([dx, dy],
-                                                        dtype=np.int8)
+                                # (x, y) is the lake. We are at (nx, ny) next to it.
+                                # Set best move to [0, 0] to stop/interact here.
+                                best[nx, ny] = np.array([0, 0], dtype=np.int8)
                             else:
-                                # Further away: Inherit the best step from the previous node
-                                best[nx, ny] = best[x, y]
+                                # (x, y) is a safe path tile.
+                                # Point backwards to it: (-dx, -dy).
+                                best[nx, ny] = np.array([-dx, -dy],
+                                                        dtype=np.int8)
                             q.append((nx, ny))
 
             lake_dist.append(dist)
@@ -183,13 +184,13 @@ class ASPTransformer:
         # lake timer
         line = ""
         for i, c in enumerate(state.lake_timer):
-            line += f"lake_timer({i}, {c}, 0)."
+            line += f"lake_timer({i}, {c+1}, 0)."
         lines.append(line)
 
         # grass timer
         line = ""
         for i, c in enumerate(state.grass_timer):
-            line += f"grass_timer({i}, {c}, 0)."
+            line += f"grass_timer({i}, {c+1}, 0)."
         lines.append(line)
 
 
@@ -208,7 +209,7 @@ class ASPTransformer:
         lines.append(f"agent({state.agent[0]}, {state.agent[1]}, 0).")
 
         for s in sips:
-            lines.append(f"ctd({sips[s][0]}, {sips[s][1]}).")
+            lines.append(f"ctd({sips[s][0]}, {s}, {sips[s][1]}).")
 
         # check the frogs in the "sphere" of the agent
         # agent window
@@ -366,7 +367,7 @@ class ASPTransformer:
         lines.append(f"agent({state.agent[0]}, {state.agent[1]}, 0).")
 
         for s in sips:
-            lines.append(f"ctd({sips[s][0]}, {sips[s][1]}).")
+            lines.append(f"ctd({sips[s][0]}, {s}, {sips[s][1]}).")
 
         for i, a in enumerate(actions):
             lines.append(f"action({a}, {i}).")

env/GardenerEnv.py
@@ -148,6 +148,25 @@ class GardenerEnv(gym.Env):
         lake_positions = self.np_random.choice(list(all_positions),
                                                size=len(self._state.lakes),
                                                replace=False)
+        #easy
+        #lake_positions = []
+        #while len(lake_positions) < len(self._state.lakes):
+        #    lake_position = self.np_random.choice(list(all_positions),
+        #                                          size=1,
+        #                                          replace=False)
+        #    for lake_pos in lake_position:
+        #        lake_positions.append(tuple(lake_pos))
+        #        all_positions.discard(tuple(lake_pos))
+        #        lx, ly = lake_pos
+        #        neighbors = [
+        #            (lx + 1, ly), (lx - 1, ly), (lx + 1, ly + 1),
+        #            (lx - 1, ly - 1),
+        #            (lx, ly + 1), (lx, ly - 1), (lx - 1, ly + 1),
+        #            (lx + 1, ly - 1)
+        #        ]
+        #        for nx, ny in neighbors:
+        #            if 0 <= nx < self._state.size and 0 <= ny < self._state.size:
+        #                all_positions.discard((nx, ny))
 
         for lake_pos in lake_positions:
             all_positions.discard(tuple(lake_pos))
@@ -350,9 +369,10 @@ class GardenerEnv(gym.Env):
                 action = q_agent.getAction(state)
             self._dynamics.move_agent(state, action)
             self._dynamics.move_frogs(state)
+            self.update_env(state, 0 )
             executed_actions.append(action)
-            if np.any(np.all(state.agent == state.frogs, axis=1)):
-                return False, executed_actions
+            #if np.any(np.all(state.agent == state.frogs, axis=1)):
+            #    return False, executed_actions
         return True, executed_actions
 
 
@@ -495,7 +515,20 @@ class GardenerEnv(gym.Env):
                 state.lakes_full[i] = False
                 state.lake_timer[i] = state.lake_respawn
                 for f,(c,r) in enumerate(state.frogs):
-                    if abs(c - lx) + abs(r - ly) == 1:
+                    prox = False
+                    if not state.dead_frogs[f]:
+                        if abs(lx - c) + abs(
+                                ly - r) == 1:
+                            prox = True
+                        elif abs(lx - c) + abs(
+                                ly - r) == 2 and abs(
+                            lx - c) == 1:
+                            prox = True
+                        elif abs(lx - c) + abs(
+                                ly - r) == 2 and abs(
+                            ly - r) == 1:
+                            prox = True
+                    if prox:
                         state.frog_timer[f] = 5
         return reward
 